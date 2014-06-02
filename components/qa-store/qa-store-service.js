8987f76d600bd33cc9804ef411863446fd1e7355
Erin Altenhof-Long <ealtenhof@google.com>
2014-06-02 14:27:34 -0700

    feat(getChildren): implement getChildren() of qaStoreService

--- components/qa-store/qa-store-service.js
+++ components/qa-store/qa-store-service.js
@@ -2,14 +2,42 @@ angular.module('qaStore').
   service('qaStoreService', function() {
   	var questions = {
   		a: {
+  			id: 'a',
   			parent: null, 
-  			children: ['b', 'c'], 
+  			children: ['b', 'c', 'd'], 
   			question: null
   		}, 
-  		b: {}
+  		b: {
+  			id: 'b',
+  			parent: 'a'
+  		},
+  		c: {
+  			id: 'c',
+  			parent: 'a'
+  		},
+  		d: {
+  			id: 'd',
+  			parent: 'a'
+  		},
   	};
 
   	this.getRoot = function() {
   		return questions.a;
   	};
+
+  	this.getChildren = function(parentNode){
+  		if(!parentNode || !parentNode.children || !Array.isArray(parentNode.children)){
+  		  throw new Error('Parent node must have children');
+  		}
+  		var children = [];
+  		parentNode.children.forEach(function(id) {
+        var child = questions[id];
+        if(!child){
+          throw new Error('Child does not exist for id: ' + id);   
+        }
+  			children.push(questions[id]);
+  		})
+  		return children;
+  	};
+
   });
--- components/qa-store/qa-store_test.js
+++ components/qa-store/qa-store_test.js
@@ -19,7 +19,34 @@ describe('qaStore', function() {
 				expect(rootNode.children.length).toBeGreaterThan(0);
 				expect(rootNode.question).toBe(null);
 			});
-		});	
+		});
+
+
+    describe('.getChildren()', function(){
+      it('should return an array of nodes', function(){
+        var rootNode = qaStoreService.getRoot();
+        var children = qaStoreService.getChildren(rootNode);
+        expect(Array.isArray(children)).toBe(true);
+        expect(children[0].parent).toBe('a');
+        expect(children[1].parent).toBe('a');
+        expect(children[2].parent).toBe('a');
+      });
+
+
+      it('should throw if parent node does not have a children array', function(){
+        expect(qaStoreService.getChildren).toThrow(new Error('Parent node must have children'));
+      });
+
+
+      it('should throw if child does not exist for an id', function(){
+        var node = {
+          children : ['fakeId']
+        };
+        expect(function() { 
+          qaStoreService.getChildren(node);
+        }).toThrow('Child does not exist for id: fakeId');
+      });
+    })	
 	});
 });
 
