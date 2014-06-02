bfa0f205f56261019965f2ebef8ffc672ae70faa
Erin Altenhof-Long <ealtenhof@google.com>
2014-06-02 11:28:11 -0700

    chore(karma): set up config and dependencies for karma

---
+++ karma.conf.js
@@ -0,0 +1,15 @@
+// Karma configuration
+// Generated on %DATE%
+
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine'],
+    files: [
+      'components/**/*.js'
+    ],
+    exclude: [
+    ],
+    preprocessors: undefined,
+    browsers: ['Chrome']
+  });
+};
