e3e74f5cafd151b67e1ad170a70967b994ff7cf2
Erin Altenhof-Long <ealtenhof@google.com>
2014-06-04 08:52:09 -0700

    chore(qaStore): fill questions dictionary with current values

--- components/qa-store/qa-store-service.js
+++ components/qa-store/qa-store-service.js
@@ -1,12 +1,74 @@
 angular.module('qaStore').
   service('qaStoreService', function() {
-    var root, iWantToHelp;
+    var root, iWantToHelp, helpOtherDevs, helpImproveAngular,
+    helpMakeComponents, helpUpdateDocs, helpUpdateTutorial, helpShareResources,
+    helpTriage, helpPRflags, helpSupportComponents, helpShareComponents;
 
+    helpShareComponents = {
+      url: 'help-support-components',
+      backUrl: 'help-make-components',
+      choices: [null],
+      question: 'I can help to share components I have written.'
+    };
+    helpSupportComponents = {
+      url: 'help-support-components',
+      backUrl: 'help-make-components',
+      choices: [null],
+      question: 'I can help to support current components.'
+    };
+    helpPRflags = {
+      url: 'help-pr-flags',
+      backUrl: 'help-improve-angular',
+      choices: [null],
+      question: 'I can help with PR flags.'
+    };
+    helpTriage = {
+      url: 'help-triage',
+      backUrl: 'help-improve-angular',
+      choices: [null],
+      question: 'I can help triage issues.'
+    };
+    helpShareResources = {
+      url: 'help-share-resources',
+      backUrl: 'help-other-devs',
+      choices: [null],
+      question: 'I can help share resources that helped me understand Angular.'
+    };
+    helpUpdateTutorial = {
+      url: 'help-update-tutorial',
+      backUrl: 'help-other-devs',
+      choices: [null],
+      question: 'I can help update Angular\'s tutorial.'
+    };
+    helpUpdateDocs = {
+      url: 'help-update-docs',
+      backUrl: 'help-other-devs',
+      choices: [null],
+      question: 'I can help update Angular\'s documentation.'
+    };
+    helpMakeComponents = {
+      url: 'help-make-components',
+      backUrl: 'i-want-to-help',
+      choices: [helpShareComponents, helpShareComponents],
+      question: 'I want to help make components for Angular.'
+    };
+    helpImproveAngular = {
+      url: 'help-improve-angular',
+      backUrl: 'i-want-to-help',
+      choices: [helpTriage, helpPRflags],
+      question: 'I want to help improve Angular itself.'
+    };
+    helpOtherDevs = {
+      url: 'help-other-devs',
+      backUrl: 'i-want-to-help',
+      choices: [helpUpdateDocs, helpUpdateTutorial, helpShareResources],
+      question: 'I want to help other developers understand Angular.'
+    };
     iWantToHelp = {
       url: 'i-want-to-help',
       backUrl: '/',
-      choices: [null],
-      question: 'I want to help other developers understand Angular.'
+      choices: [helpOtherDevs, helpImproveAngular, helpMakeComponents],
+      question: 'I want to help with Angular.'
     };
     root = {
       url: '/',
@@ -18,7 +80,17 @@ angular.module('qaStore').
 
     this.questions_ = {
       null: root,
-      'i-want-to-help': iWantToHelp
+      'i-want-to-help': iWantToHelp,
+      'help-other-devs': helpOtherDevs,
+      'help-improve-angular': helpImproveAngular,
+      'help-make-components': helpMakeComponents,
+      'help-update-docs': helpUpdateDocs,
+      'help-update-tutorial': helpUpdateTutorial,
+      'help-share-resources': helpShareResources,
+      'help-triage': helpTriage,
+      'help-pr-flags': helpPRflags,
+      'help-support-components': helpSupportComponents,
+      'help-share-components': helpShareComponents
     };
 
     this.__defineSetter__('currentNode', function(node) {
