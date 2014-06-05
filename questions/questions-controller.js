d2fefc8c4bd1cf71bc228cef38d9e1d79ee108eb
Erin Altenhof-Long <ealtenhof@google.com>
2014-06-05 14:32:00 -0700

    feat(haShareResources): add filtering by type for share resources view

--- questions/questions-controller.js
+++ questions/questions-controller.js
@@ -2,7 +2,6 @@ angular.module('haQuestions').
 controller('QuestionsController', ['qaStoreService', '$routeParams', '$window',
     function(qaStoreService, $routeParams, $window) {
       var node = qaStoreService.getByUrl($routeParams.url);
-      console.log(node);
       if(node === undefined){
         $window.location.href = '#questions/i-want-to-help';
         node = qaStoreService.getByUrl('i-want-to-help');
